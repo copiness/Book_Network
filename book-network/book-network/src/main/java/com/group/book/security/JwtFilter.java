eece10cdcebc71adc0476a7a05567093fda1bdc7
copiness <aklantabhukta775@gmail.com>
2024-11-15 12:44:17 +0530

    Revert "Merge pull request #1 from jwjjhehsu/main"
    This reverts commit 28539da2e548c16fcf2343f3a1c668244ecacbf0.

--- book-network/book-network/src/main/java/com/group/book/security/JwtFilter.java
+++ book-network/book-network/src/main/java/com/group/book/security/JwtFilter.java
@@ -7,11 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -24,7 +19,6 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
 
 
 
@@ -47,18 +41,5 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
-        if (userEmail == null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-            if(jwtService.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        filterChain.doFilter(request, response);
     }
 }
